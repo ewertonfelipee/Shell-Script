a1b47260eea64b48f2bc37633a7afd9d899e683e
ewertonfelipee <ewertonfmarinho@protonmail.ch>
2022-08-02 11:42:14 -0300

    First Shell Script

---
+++ PrimeiroScript.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+clear
+echo "===== Meu Primeiro Script ====="
+echo ""
+echo -n "Exibir data e hora atual:"
+date
+echo "==================================="
+echo "Listagem dos alunos"
+sort ../CursoShellScript/arquivos/alunos2.txt
